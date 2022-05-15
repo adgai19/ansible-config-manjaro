82c42eb79f76e4348f9ab98df5efdb1f00d7303f
adgai19 <gaitondeaditya@gmail.com>
2022-05-15 17:30:38 +0530

    a lot more stuff

---
+++ bin/dotfiles.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# Paths
+DOTFILES_DIR="$HOME/.dotfiles"
+if ! [ -x "$(command -v ansible)" ]; then
+  sudo pacman -S ansible
+fi
+
+if ! [[ -d "$DOTFILES_DIR" ]]; then
+  git clone "git@github.com:adgai19/ansible-config-manjaro.git" "$DOTFILES_DIR"
+else
+  git -C "$DOTFILES_DIR" pull
+fi
+
+if ! [ -x "$(command -v dotfiles.sh)" ]; then
+  ln -s "$DOTFILES_DIR/bin/dotfiles.sh" "$HOME/.local/bin"
+fi
+cd "$DOTFILES_DIR"
+
+ansible-galaxy install -r requirements.yml
+
+ansible-playbook --diff "$DOTFILES_DIR/main.yml"
